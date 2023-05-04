8b2bc05af99a4c0a54c14706b38fc1b4ba329a7d
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-05-04 17:18:06 +0600

    firebase install and authprovider add

---
+++ src/firebase/firebas.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcbWc3NPDhvauKuwZ5KFBXv84uGcpd-PU",
+  authDomain: "cook-chef-99c0b.firebaseapp.com",
+  projectId: "cook-chef-99c0b",
+  storageBucket: "cook-chef-99c0b.appspot.com",
+  messagingSenderId: "199935775133",
+  appId: "1:199935775133:web:e9c29a0f44e459d685df74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './RouterProvider/RouterProvider';
+import AuthProvider from './provider/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
